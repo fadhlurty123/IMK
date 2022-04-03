f9c009cb04285da2a67dc402a2d816b11c233054
Fahrul Thariq Fadillah <fahrul.thoriq@gmail.com>
2022-04-03 21:46:17 +0700

    add apk+program+code

---
+++ IMK-2022/Assets/Scripts/UI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
